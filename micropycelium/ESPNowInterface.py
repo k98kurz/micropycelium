cdf98e49c909e4153eca593b57ff021f7ad1fbe3
Jonathan Voss <k98kurz@gmail.com>
2024-08-26 00:00:21 -0400

    changed for namespacing because it all goes into one file

--- micropycelium/ESPNowInterface.py
+++ micropycelium/ESPNowInterface.py
@@ -1,4 +1,4 @@
-from Packager import Interface, Datagram
+from Packager import Interface, Datagram, Packager
 import network
 import espnow
 
@@ -12,30 +12,32 @@ e = espnow.ESPNow()
 e.active(True)
 e.add_peer(b'\xff\xff\xff\xff\xff\xff')
 
-def config(intrfc: Interface, data: dict):
+def config_espnwintrfc(intrfc: Interface, data: dict):
     for k,v in data.items():
         _config[k] = v
 
-def recv(intrfc: Interface) -> bytes|None:
+def recv_espnwintrfc(intrfc: Interface) -> bytes|None:
     res = e.recv(0)
     if res and res[0]:
         return Datagram(res[1], intrfc.id, res[0])
 
-def send(datagram: Datagram):
+def send_espnwintrfc(datagram: Datagram):
     if datagram.addr not in [p[0] for p in e.get_peers()]:
         e.add_peer(datagram.addr)
     e.send(datagram.addr, datagram.data, False)
 
-def broadcast(datagram: Datagram):
+def broadcast_espnwintrfc(datagram: Datagram):
     e.send(b'\xff\xff\xff\xff\xff\xff', datagram.data, False)
 
 
 ESPNowInterface = Interface(
     name='espnow',
     bitrate=12_000_000,
-    configure=config,
+    configure=config_espnwintrfc,
     supported_schemas=[i for i in range(0, 11)],
-    receive_func=recv,
-    send_func=send,
-    broadcast_func=broadcast,
+    receive_func=recv_espnwintrfc,
+    send_func=send_espnwintrfc,
+    broadcast_func=broadcast_espnwintrfc,
 )
+
+Packager.add_interface(ESPNowInterface)
