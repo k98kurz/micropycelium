4c079996591fe6cddcc34493379aa140d5ad255f
Jonathan Voss <k98kurz@gmail.com>
2024-08-26 00:05:25 -0400

    more mocks

--- machine/__init__.py
+++ machine/__init__.py
@@ -7,3 +7,18 @@ def unique_id() -> bytes:
 
 def lightsleep(ms):
     sleep(ms/1000)
+
+class Pin:
+    _val: int
+    _mode: int
+    IN = 1
+    OUT = 3
+    def __init__(self, num: int, mode: int = 1) -> None:
+        self._val = 0
+        self._mode = mode
+        pass
+
+    def value(self, val: int = None):
+        if val is None:
+            return self._val
+        self._val = val
