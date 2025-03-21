053668c858a05fb3fc56c94a667246afa6cba95a
Jonathan Voss <k98kurz@gmail.com>
2025-03-20 22:29:44 -0400

    small change to main.py

--- devices/main.py
+++ devices/main.py
@@ -1,7 +1,7 @@
 from micropycelium import Packager, Address
 from mpnode import (
-    start, debug_name, Gossip, SpanningTree, Ping,
-    DebugApp, DebugOp, ping_cb
+    start, debug_name, Gossip, SpanningTree, Ping, DebugApp, DebugOp,
+    ping_cb, monitor_btn, bloop, blink, memrloop,
 )
 import machine
 
@@ -10,4 +10,11 @@ Packager.add_hook('add_route', debug_name('Packager.add_route'))
 Packager.add_hook('set_addr', debug_name('Packager.set_addr'))
 Packager.add_hook('deliver:checksum_failed', debug_name('Packager.deliver:checksum_failed'))
 Packager.add_hook('deliver:receive_failed', debug_name('Packager.deliver:receive_failed'))
+
+# default start
 start()
+
+# custom start with additional tasks
+# start([
+#     # additional custom tasks here
+# ])
